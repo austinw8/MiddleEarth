7d00fbdbe24d9e1796bfd026dedf368b8db8532a
bridger-norman <littlebnorm@gmail.com>
2024-08-07 14:30:08 -0600

    working code for a single documentation file

--- R/data_documentation.R
+++ R/data_documentation.R
@@ -99,3 +99,298 @@
 #' @source <https://github.com/jvangeld/ME-GIS>
 #'
 'contours'
+
+#' Middle Earth forest data
+#'
+#' Geospatial shapefiles of forests throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `forests`
+#' A data frame with 2033 rows and 8 columns:
+#' \describe{
+#'   \item{NAME}{forest name}
+#'   \item{TYPE}{forest type}
+#'   \item{PERIMETER}{forest perimeter}
+#'   \item{ENCLOSED_A}{forest area}
+#'   \item{ISLAND_ARE}{island area}
+#'   \item{NUMBER}{forest data ID number}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'forests'
+
+#' Middle Earth hamlet data
+#'
+#' Geospatial shapefiles of hamlets (small settlements) throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `hamlets`
+#' A data frame with 20 rows and 4 columns:
+#' \describe{
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'hamlets'
+
+#' Middle Earth helper contour line data
+#'
+#' Geospatial shapefiles of helper contour lines throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `helper_contours`
+#' A data frame with 19 rows and 8 columns:
+#' \describe{
+#'   \item{LENGTH}{contour line length}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'helper_contours'
+
+#' Middle Earth hill data
+#'
+#' Geospatial shapefiles of hills throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `hills`
+#' A data frame with 2179 rows and 7 columns:
+#' \describe{
+#'   \item{PERIMETER}{hill perimeter}
+#'   \item{ENCLOSED_A}{enclosed hill area}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'hills'
+
+
+#' Middle Earth lakes data
+#'
+#' Geospatial shapefiles of lakes throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `lakes`
+#' A data frame with 256 rows and 7 columns:
+#' \describe{
+#'   \item{NAME}{lake name}
+#'   \item{PERIMETER}{lake perimeter}
+#'   \item{ENCLOSED_A}{lake area}
+#'   \item{ISLAND_ARE}{island area}
+#'   \item{type}{type}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'lakes'
+
+#' Middle Earth mountains data
+#'
+#' Geospatial shapefiles of mountains throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `mountains`
+#' A data frame with 10 rows and 3 columns:
+#' \describe{
+#'   \item{name}{mountain range name}
+#'   \item{fontsizeKm}{fontzize in km}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'mountains'
+
+#' Middle Earth place name data
+#'
+#' Geospatial shapefiles of place names throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `placenames`
+#' A data frame with 785 rows and 11 columns:
+#' \describe{
+#'   \item{LAYER}{place name type}
+#'   \item{NAME}{place name}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'placenames'
+
+#' Middle Earth regions data
+#'
+#' Geospatial shapefiles of regions throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `regions`
+#' A data frame with 14 rows and 3 columns:
+#' \describe{
+#'   \item{name}{region name}
+#'   \item{fontsizeKm}{font size in km}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'regions'
+
+
+#' Middle Earth rivers data
+#'
+#' Geospatial shapefiles of rivers throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `rivers`
+#' A data frame with 2497 rows and 7 columns:
+#' \describe{
+#'   \item{LENGTH}{river length}
+#'   \item{TYPE}{river type}
+#'   \item{NAME}{river name}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'rivers'
+
+#' Middle Earth road data
+#'
+#' Geospatial shapefiles of roads throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `roads`
+#' A data frame with 1010 rows and 4 columns:
+#' \describe{
+#'   \item{NAME}{road name}
+#'   \item{TYPE}{road type}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'roads'
+
+#' Middle Earth ruins data
+#'
+#' Geospatial shapefiles of ruins throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `ruins`
+#' A data frame with 237 rows and 3 columns:
+#' \describe{
+#'   \item{NAME}{ruin name}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'ruins'
+
+#' Middle Earth stonefields data
+#'
+#' Geospatial shapefiles of stonefields throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `stonefields`
+#' A data frame with 7 rows and 6 columns:
+#' \describe{
+#'   \item{PERIMETER}{stonefield perimeter}
+#'   \item{ENCLOSED_A}{stonefield enclosed area}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'stonefields'
+
+
+#' Middle Earth towers and keeps data
+#'
+#' Geospatial shapefiles of towers and keeps throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `towers_and_keeps`
+#' A data frame with 202 rows and 3 columns:
+#' \describe{
+#'   \item{NAME}{tower or keep name}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'towers_and_keeps'
+
+#' Middle Earth towns data
+#'
+#' Geospatial shapefiles of towns throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `towns`
+#' A data frame with 425 rows and 4 columns:
+#' \describe{
+#'   \item{TYPE}{town type}
+#'   \item{NAME}{toen name}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'towns'
+
+
+#' Middle Earth vulcanism data
+#'
+#' Geospatial shapefiles of volcanos throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `vulcanism`
+#' A data frame with 110 rows and 7 columns:
+#' \describe{
+#'   \item{NAME}{volcano name}
+#'   \item{PERIMETER}{volcano perimeter}
+#'   \item{ENCLOSED_A}{volcano area}
+#'   \item{ISLAND_ARE}{island area}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'vulcanism'
+
+#' Middle Earth wetlands data
+#'
+#' Geospatial shapefiles of wetlands throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `wetlands`
+#' A data frame with 181 rows and 7 columns:
+#' \describe{
+#'   \item{NAME}{wetlands name}
+#'   \item{PERIMETER}{wetlands perimeter}
+#'   \item{ENCLOSED_A}{wetlands area}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'wetlands'
+
+
+
+
+
--- data-raw/beacons.R
+++ data-raw/beacons.R
@@ -1,3 +1,7 @@
 ## code to prepare `DATASET` dataset goes here
 
-usethis::use_data(DATASET, overwrite = TRUE)
+beacons <- sf::read_sf("C:/Users/austi/OneDrive/Desktop/R/data/ME-GIS/Beacons.shp") |>
+  dplyr::mutate(across(where(is.character), ~iconv(., from = "ISO-8859-1", to = "UTF-8")))
+
+usethis::use_data(beacons, overwrite = TRUE)
+
