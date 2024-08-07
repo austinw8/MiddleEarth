7f423691c872896111f814a4d1582d6b812c75b7
bridger-norman <littlebnorm@gmail.com>
2024-08-07 14:03:33 -0600

    initial commit

---
+++ R/data_documentation.R
@@ -0,0 +1,101 @@
+#' Middle Earth beacon data
+#'
+#' Geospatial shapefiles of beacons throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `beacons`
+#' A data frame with 150 rows and 2 columns:
+#' \describe{
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+'beacons'
+
+#' Middle Earth citadels data
+#'
+#' Geospatial shapefiles of citadels throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `citadels`
+#' A data frame with 119 rows and 3 columns:
+#' \describe{
+#'   \item{NAME}{citadel name}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'citadels'
+
+#' Middle Earth cities data
+#'
+#' Geospatial shapefiles of cities throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `cities`
+#' A data frame with 30 rows and 4 columns:
+#' \describe{
+#'   \item{TYPE}{city type}
+#'   \item{NAME}{city name}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'cities'
+
+#' Middle Earth coastline data
+#'
+#' Geospatial shapefiles of coastlines throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `coastline`
+#' A data frame with 110 rows and 14 columns:
+#' \describe{
+#'   \item{LENGTH}{length of coastline}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'coastline'
+
+#' Middle Earth regional name data
+#'
+#' Geospatial shapefiles of regional names throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `combined_placenames`
+#' A data frame with 785 rows and 11 columns:
+#' \describe{
+#'   \item{LAYER}{region type}
+#'   \item{NAME}{name of region}
+#'   \item{FONT_NAME}{name of font used}
+#'   \item{FONT_COLOR}{color of font used}
+#'   \item{ORIGIN}{data source}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'combined_placenames'
+
+#' Middle Earth contour lines data
+#'
+#' Geospatial shapefiles of countour lines throughout Middle Earth
+#' Report ...
+#'
+#' @format ## `contours`
+#' A data frame with 4266 rows and 14 columns:
+#' \describe{
+#'   \item{LENGTH}{contour length}
+#'   \item{geometry}{geometry}
+#'   ...
+#' }
+#' @source <https://github.com/jvangeld/ME-GIS>
+#'
+'contours'
